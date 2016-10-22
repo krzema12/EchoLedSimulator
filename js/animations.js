e929d06e99ece2ec0eabc74b8d1da878cf0c24f3
Piotr Krzeminski <piotr@krzeminski.it>
2016-10-22 12:40:04 +0200

    Make the animation colorful (HSV -> RGB)

--- js/animations.js
+++ js/animations.js
@@ -9,15 +9,48 @@ $(document).ready(function() {
 
     // ========== Start registering animation here. ===========
 
+    /* accepts parameters
+     * h  Object = {h:x, s:y, v:z}
+     * OR
+     * h, s, v
+     * h is probably in degrees
+     * Source: http://stackoverflow.com/questions/17242144/javascript-convert-hsb-hsv-color-to-rgb-accurately
+    */
+    var hsvToRgb = function (h, s, v) {
+        var r, g, b, i, f, p, q, t;
+        if (arguments.length === 1) {
+            s = h.s, v = h.v, h = h.h;
+        }
+        i = Math.floor(h * 6);
+        f = h * 6 - i;
+        p = v * (1 - s);
+        q = v * (1 - f * s);
+        t = v * (1 - (1 - f) * s);
+        switch (i % 6) {
+            case 0: r = v, g = t, b = p; break;
+            case 1: r = q, g = v, b = p; break;
+            case 2: r = p, g = v, b = t; break;
+            case 3: r = p, g = q, b = v; break;
+            case 4: r = t, g = p, b = v; break;
+            case 5: r = v, g = p, b = q; break;
+        }
+        return {
+            r: Math.round(r * 255),
+            g: Math.round(g * 255),
+            b: Math.round(b * 255)
+        };
+    };
+
     registerAnimation({
         name: 'Rotating colorful points',
-        duration: 700,
+        duration: 4000,
         step: 0.01,
         beforeAnimation: function(ledIndex, currentLed) {
-            currentLed.color = { red: 0, green: 255, blue: 0 };
+            var hsv = hsvToRgb(ledIndex/12, 1.0, 1.0);
+            currentLed.color = { red: hsv.r, green: hsv.g, blue: hsv.b };
         },
         descriptionFunction: function(progress, ledIndex, currentLed) {
-            var currentLedIndexToShine = parseInt(progress*12.0);
+            var currentLedIndexToShine = parseInt(progress*4*12.0) % 12;
 
             if (ledIndex == currentLedIndexToShine || ledIndex == (currentLedIndexToShine + 6) || ledIndex == (currentLedIndexToShine - 6)) {
                 currentLed.brightness = 1.0;
